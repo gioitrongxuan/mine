4703b3bf35ae567058142fb095139c768c53145c
Duong Gioi <118875482+DGioi@users.noreply.github.com>
2023-11-08 19:00:34 +0700

    add home page

---
+++ javascript/animation.js
@@ -0,0 +1,63 @@
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("myButton").addEventListener("click", function() {
+    setTimeout(function() {
+      window.open("https://example.com","_blank"); // Đường dẫn của liên kết cần mở
+    }, 1000); // 1000ms = 1 giây
+  })
+});
+
+
+      
+// Tự động chuyển đổi lớp để hiển thị nội dung sau khi trang "index" đã hoàn thành
+window.addEventListener('DOMContentLoaded', function() {
+  var content = document.getElementById('preload');
+  setTimeout(function() {
+    content.style.transform = 'translateX(0)'; // Di chuyển nội dung về vị trí ban đầu
+    setTimeout(function() {
+      content.style.transform = 'translateX(100%)'; // Di chuyển nội dung lại ra bên trái
+    }, 2000); 
+  }, 1500); // Tạm dừng 100ms trước khi thực hiện hiệu ứng lần đầu
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+var popup = document.querySelector('.popup2');
+setTimeout(function() {
+    popup.style.display = 'none';
+}, 2500); // Thời gian hiển thị popup (đơn vị: milliseconds)
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+var popup = document.querySelector('.popup0');
+setTimeout(function() {
+    popup.style.display = 'none';
+}, 1800); // Thời gian hiển thị popup (đơn vị: milliseconds)
+});
+window.addEventListener('DOMContentLoaded', function() {
+var popup = document.getElementById('popup1');
+popup.style.display = 'block';
+});
+
+// Tự động định vị đến mục có id "home" khi trang tải
+// Tải trang "home.html" khi trang đã tải xong
+window.addEventListener('DOMContentLoaded', function() {
+loadContent("home");
+});
+
+// Xử lý khi chọn một phần khác
+function selectSection(section) {
+// Ẩn phần "home" bằng cách đặt nội dung thành rỗng
+var contentDiv = document.getElementById("content");
+contentDiv.innerHTML = "";
+
+// Tải và hiển thị phần tương ứng
+loadContent(section);
+}
+
+var typed = new Typed(".typing",
+{
+    strings:["HUSTのHEDSPIの学生","ウェブ開発者","ウェブデザイナー","アンドロイドのアプリ開発者"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop:true
+
+})
---
+++ javascript/script.js
@@ -0,0 +1,66 @@
+function loadContent(page) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var contentDiv = document.getElementById("content");
+      contentDiv.innerHTML = this.responseText;
+
+      // Tải và áp dụng JavaScript
+      var scriptTags = contentDiv.getElementsByTagName("script");
+      for (var i = 0; i < scriptTags.length; i++) {
+        var scriptTag = scriptTags[i];
+        var newScript = document.createElement("script");
+        newScript.src = scriptTag.src;
+        document.head.appendChild(newScript);
+      }
+
+      // Tải và áp dụng CSS
+      var linkTags = contentDiv.getElementsByTagName("link");
+      for (var j = 0; j < linkTags.length; j++) {
+        var linkTag = linkTags[j];
+        if (linkTag.rel === "stylesheet") {
+          var newLink = document.createElement("link");
+          newLink.rel = "stylesheet";
+          newLink.href = linkTag.href;
+          document.head.appendChild(newLink);
+        }
+      }
+    }
+  };
+  xhttp.open("GET", page + ".html", true);
+  xhttp.send();
+}
+// function loadContent(page) {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       var contentDiv = document.getElementById("content");
+//       contentDiv.innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", page + ".html", true);
+//   xhttp.send();
+// }
+    
+    // Hiển thị div có id tương ứng với id truyền vào
+    var selectedDiv = document.getElementById(page);
+    if (selectedDiv) {
+      selectedDiv.style.display = "block";
+    }
+    
+    // Lặp qua các mục trong Navbar và loại bỏ lớp CSS .nav-selected
+    var navItems = document.querySelectorAll(".navbar-nav .nav-link");
+    navItems.forEach(function(item) {
+      item.classList.remove("nav-selected");
+    });
+    
+    // Thêm lớp CSS .nav-selected cho mục được chọn
+    var selectedNavItem = document.querySelector(".navbar-nav a[href='" + page + "']");
+    if (selectedNavItem) {
+      selectedNavItem.classList.add("nav-selected");
+    }
+
+
+
+    
+  
